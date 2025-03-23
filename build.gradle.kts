4d2f97c120d5712f42793b86d95d1e02f3033897
DavidMoralesAguilarUD <davidtrokten@gmail.com>
2025-03-22 20:14:03 -0500

    Initial Structure Configuration

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.4.4"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "sura.com.co"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+
+	implementation ("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
+	implementation ("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("org.jetbrains:annotations:24.0.0")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.projectreactor:reactor-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	compileOnly ("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
